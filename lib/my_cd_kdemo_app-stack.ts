ff1aca803d92a6fce990efc20e97067cfa9590b4
ramezani <mh486520@dal.ca>
2024-06-15 16:54:04 -0300

    CDK-demo-l1-l2-S3

--- lib/my_cd_kdemo_app-stack.ts
+++ lib/my_cd_kdemo_app-stack.ts
@@ -1,4 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket, EventType } from 'aws-cdk-lib/aws-s3';
+import { SqsDestination } from 'aws-cdk-lib/aws-s3-notifications';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +9,23 @@ export class MyCdKdemoAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // L1 construct S3
+    const level1S3Bucket = new CfnBucket(this, 'MyFirstLevel1ConstructBucket', {
+      versioningConfiguration: {
+        status: "Enabled"
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyCdKdemoAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // L2 construct S3
+    const level2S3Bucket = new Bucket(this, 'MyFirstLevel2ConstructBucket', {
+      bucketName: "mohammad-myfirstlevel2constructbucket",
+      versioned: true
+    });
+
+    const queue = new Queue(this, 'MyQueue', {
+      queueName: 'MyQueue'
+    });
+
+    level2S3Bucket.addEventNotification(EventType.OBJECT_CREATED, new SqsDestination(queue))
   }
 }
